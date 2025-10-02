937748cf00f923cb95a6ba3c01131e855fc2f0dc
AndresAusecha <andres.mosquera@arrive.com>
2025-10-02 11:54:52 -0500

    add files for the first version

---
+++ src/main/java/co/aamsis/concurrent/spring/ConcurrentSpringApplication.java
@@ -0,0 +1,13 @@
+package co.aamsis.concurrent.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrentSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcurrentSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/aamsis/concurrent/spring/ConcurrentSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package co.aamsis.concurrent.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrentSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
